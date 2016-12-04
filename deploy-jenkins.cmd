dcfb9649741ca52f12194ba6e86ecfb38e2205b5
Pierre <PierreBtz@users.noreply.github.com>
2016-12-04 18:50:40 +0100

    Add jenkins service to the docker compose

--- deploy-jenkins.cmd
+++
@@ -1,1 +0,0 @@
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home --name jenkins_dev -p 80:8080 jenkinsdev
--- deploy-jenkins.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home -p 80:8080 jenkinsdev
